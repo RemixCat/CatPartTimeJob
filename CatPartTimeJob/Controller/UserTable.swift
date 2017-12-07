0e7fcab4bb18adad7c97033f6298dfbff56b40f4
Remixcat <Remixcat@126.com>
2017-12-07 12:01:15 +0800

    个人中心

---
+++ CatPartTimeJob/Controller/UserTable.swift
@@ -0,0 +1,21 @@
+//
+//  UserTable.swift
+//  CatPartTimeJob
+//
+//  Created by admins on 2017/12/7.
+//  Copyright © 2017年 Remixcat. All rights reserved.
+//
+
+import UIKit
+
+class UserTable: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
